377466e5517fba36e5df8f5680f8a7e13a1507c1
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-06-21 22:56:05 -0300

    Cadastando as APIs de cospobre

--- inscricao/urls.py
+++ inscricao/urls.py
@@ -1,5 +1,6 @@
 from inscricao.views import Inscricao, Participantes, ParticipantesDetalhados
 from inscricao.views import limparPresentes, InscricaoCospobre
+from inscricao.views import Cospobres, CospobreDetalhados
 from django.urls import path
 
 urlpatterns = [
@@ -7,5 +8,7 @@ urlpatterns = [
     path('cospobre', InscricaoCospobre),
     path('v1/participantes/<int:edicao>/', Participantes.as_view()),
     path('v1/participantes/<int:edicao>/<int:pk>/', ParticipantesDetalhados.as_view()),
+    path('v1/cospobre/<int:edicao>/', Cospobres.as_view()),
+    path('v1/cospobre/<int:edicao>/<int:pk>/', CospobreDetalhados.as_view()),
     path('presentes/<int:edicao>/', limparPresentes),
 ]
--- inscricao/views.py
+++ inscricao/views.py
@@ -2,7 +2,6 @@ from inscricao.models import Edicao, Inscritos, Cospobre
 from inscricao.forms import InscritosForm, CospobreForm
 from inscricao.serializers import InscritosSerializers, CospobreSerializers
 
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -60,6 +59,25 @@ class Participantes(APIView):
         inscritos.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+class Cospobres(APIView):
+    def get(self, request, edicao):
+        participantes = Cospobre.objects.filter(edicao__numero=edicao)
+        serializer = CospobreSerializers(participantes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, edicao):
+        serializer = CospobreSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, edicao):
+        participantes = Cospobre.objects.filter(edicao__numero=edicao)
+        participantes.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ParticipantesDetalhados(APIView):
     def get_object(self, edicao, pk):
         try:
@@ -83,6 +101,31 @@ class ParticipantesDetalhados(APIView):
     def delete(self, request, edicao, pk):
         inscritos = self.get_object(edicao, pk)
         inscritos.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CospobreDetalhados(APIView):
+    def get_object(self, edicao, pk):
+        try:
+            return Cospobre.objects.get(edicao__numero=edicao, pk=pk)
+        except Cospobre.DoesNotExist:
+            raise NotFound()
+
+    def get(self, request, edicao, pk):
+        participante = self.get_object(edicao, pk)
+        serializer = CospobreSerializers(participante)
+        return Response(serializer.data)
+
+    def put(self, request, edicao, pk):
+        participante = self.get_object(edicao, pk)
+        serializer = CospobreSerializers(participante, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, edicao, pk):
+        participante = self.get_object(edicao, pk)
+        participante.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @login_required(login_url='/admin/')
