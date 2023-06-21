b740cc326908ad726811b4cec2d4a096c55329af
Samuel Oliveira <s.oliveira.15@hotmail.com>
2023-06-20 22:04:51 -0300

    Configuração do banco de dados

--- cuscuzhq/settings.py
+++ cuscuzhq/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,13 +118,26 @@ WSGI_APPLICATION = 'cuscuzhq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9v55c9rkq92ph',
+        'USER': 'glguzbsccngyvq',
+        'PASSWORD': '1f248c60ca4a326a5f944d539780967a77bf82e3d72749368892e1cb9d4c4aa7',
+        'HOST': 'ec2-52-205-45-222.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age = 500)
+DATABASES['default'].Update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
