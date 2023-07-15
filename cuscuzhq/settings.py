d55359b39733d600b4f57f42e55e0a0a0f300bf5
Samuel Oliveira <s.oliveira.15@hotmail.com>
2023-07-14 23:56:24 -0300

    Alterando para o banco de dados Heroku

--- cuscuzhq/settings.py
+++ cuscuzhq/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-#import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,10 +122,11 @@ WSGI_APPLICATION = 'cuscuzhq.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 #DATABASES = {
