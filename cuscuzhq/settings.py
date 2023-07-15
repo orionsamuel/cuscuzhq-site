3ef213991dc619658a17b2b7b5889cbef46bdd24
Samuel Oliveira <s.oliveira.15@hotmail.com>
2023-07-14 23:26:55 -0300

    Criando base de dados e APIs para as notas

--- cuscuzhq/settings.py
+++ cuscuzhq/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'inscricao/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'cuscuzhq', 'media')
 MEDIA_URL = '/media/'
