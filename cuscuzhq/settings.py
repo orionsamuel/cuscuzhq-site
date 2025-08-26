a3064ceab70c94bee208134c74a422023a0ebd89
Samuel Oliveira <s.oliveira.15@hotmail.com>
2025-08-25 22:57:26 -0300

    Adicionando url a lista de sites permitidos

--- cuscuzhq/settings.py
+++ cuscuzhq/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")  # usa variável de 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost,https://cuscuzhq.herokuapp.com/, cuscuzhq.render.com").split(",")
+ALLOWED_HOSTS = os.environ.get("cuscuzhq.alwaysdata.net", "127.0.0.1", "localhost").split(",")
 
 # Application definition
 
